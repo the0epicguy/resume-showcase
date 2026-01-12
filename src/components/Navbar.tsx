cd64b523e5ea99afe6e69f815a2eea353666dd52
the0epicguy <86835596+the0epicguy@users.noreply.github.com>
2026-01-13 00:39:34 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { AboutModal } from "./AboutModal";
 
 const RESUME_URL =
-  "https://drive.google.com/file/d/1wx59B3pra-Jxu4qF7Tcl9LMZP41DESpJ/view?usp=sharing";
+  "https://drive.google.com/file/d/1C5ZzIM016jpBHf3LcJXgZ295-NRuauYm/view?usp=sharing";
 const LINKEDIN_URL = "https://www.linkedin.com/in/advait-gajewar/";
 const GITHUB_URL = "https://github.com/the0epicguy/";
 
